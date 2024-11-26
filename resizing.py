d8e1e5f04839a1be3409ac425192f7fabe10387d
alishaHong <ugyung87@gmail.com>
2024-11-26 19:23:27 +0900

    resizing

--- resizing.py
+++ resizing.py
@@ -2,13 +2,13 @@ import os
 import cv2
 
 # basepath 설정
-basepath = "C:/Users/Sesame/Desktop/food_valid_data/04017001"  # 실제 데이터가 담긴 폴더 경로로 변경해주세요.
+basepath = "C:/Users/Sesame/Desktop/food_train_image/12011003"  # 실제 데이터가 담긴 폴더 경로로 변경해주세요.
 
 # resize 목표 크기
 target_size = (640, 640)
 
 # 지원하는 이미지 확장자 목록
-image_extensions = ('.jpg', '.jpeg', '.png','.JPG','JPEG')
+image_extensions = ('.jpg', '.jpeg', '.png','.JPG','.JPEG')
 
 # basepath 아래의 모든 파일 순회
 for filename in os.listdir(basepath):
